64f3cfc986688ae091e8d83341d6143b801e8b20
Rohit <rohitjha986@gmail.com>
2019-10-07 14:16:44 +0530

    adds alert box displaying cards

--- src/list.js
+++ src/list.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Modal from "./cardModal"
 const apiKey = "f8fd58024b0cb495538a72009478e9b1";
 const token =
   "cd01d05e0ddf70aec5e7130b16463fdf99378cb2925a98b91f50b5c623f32e9b";
@@ -6,7 +7,8 @@ const token =
 class List extends Component {
   state = {
     listId: this.props.listId,
-    cards: []
+    cards: [],
+    openModal:false
   };
   componentDidMount() {
     fetch(
@@ -20,10 +22,14 @@ class List extends Component {
       .then(cardsResponseObject => cardsResponseObject.json())
       .then(cards => this.setState({ cards: cards }));
   }
+  displayAlert=(cardNumber)=>{
+    alert("Name of card = "+this.state.cards[cardNumber].name+"\nUrl = "+this.state.cards[cardNumber].url)
+  }
+  
 
   render() {
-    console.log(this.state.cards);
-    return this.state.cards.map((card,i )=> <span className="card">{card.name}</span>);
+  //return <Modal></Modal>
+    return this.state.cards.map((card,i )=> <span onClick={()=>this.displayAlert(i)} key={card.id} className="card" >.{card.name}</span>);
   }
 }
 
